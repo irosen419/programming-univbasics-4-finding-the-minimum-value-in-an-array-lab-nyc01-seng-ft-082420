d2595fcdde33f6884c2d872fed8351a376622f4c
Ian Rosen <irosen419@gmail.com>
2020-04-05 01:52:12 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -5,7 +5,13 @@
 
 def find_min_value(array)
   counter = 0
+  min_value = array[0]
   
-  
-  
+  while counter < array.length do
+    if min_value < array[counter]
+      min_value = array[counter]
+    end
+    counter += 1
+  end
+  return min_value
 end
